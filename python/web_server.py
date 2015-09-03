27eff8aed1ec51ce4ab75263f4c044fb121c6933
Van Kichline <vkichline@hotmail.com>
2015-09-02 22:02:24 -0700

    completed general query string handler template

--- python/web_server.py
+++ python/web_server.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python
-# Based on: http://www.acmesystems.it/python_httpd
+# Based on:
+#	http://www.acmesystems.it/python_httpd
+#	https://pymotw.com/2/BaseHTTPServer/
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import urlparse
 
 PORT_NUMBER = 5050
 
 class reqHandler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
+		parsed_path = urlparse.urlparse(self.path)
+		q = urlparse.parse_qsl(parsed_path.query)
+
 		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
+		
 		# Send the html message
-		self.wfile.write("Hello World!")
+		result = self.process_query(q)
+		self.wfile.write("Query Params:<br/>" + result)
 		return
+	
+	# Dummy handler for processed query (list of name/value pairs)
+	def process_query(self, query):
+		return str(query)
 
 
 #Create a web server and define the handler to manage the
